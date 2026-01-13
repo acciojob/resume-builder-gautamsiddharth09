4e14008d63ab2ce92c33125ea8fa1bc12b8e7767
Gautam kumar <gautamsiddharth2013@gmail.com>
2026-01-13 15:25:56 +0530

    refresh solution

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./app/rootReducer";
 
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
